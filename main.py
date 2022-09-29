4e402a8ffc6e5da0889fb903c84c63aefd73d92d
Leonid Zhuravlev <ekda@pop-os.localdomain>
2022-09-29 21:21:16 +0300

    Iterating and fixing numbers

--- main.py
+++ main.py
@@ -3,9 +3,17 @@ import pandas as pd
 df = pd.read_excel('book.xlsx')
 
 # For iteration:
-# for index, row in df.iterrows():
-#     print(row['Number'])
+for index, row in df.iterrows():
+    if len(str(row['Number'])) < 11:
+        v = df.iloc[index]
+        # print(v)
+        w = v.loc['Number']
+        w = '44' + str(w)
+        df.loc[[index],'Number'] = int(w)
+        
 
-print(df.loc[df['Network'] == 'EE'])
+
+# print(df.loc[df['Network'] == 'EE'])
 
 # print(df[['Number', 'Network']])
+df.to_excel('modified.xlsx', index = False)
