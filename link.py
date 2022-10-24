5a0d6f0725f17afffb5f4966c80feb5194c8b446
Leonid Zhuravlev <ekda@pop-os.localdomain>
2022-10-24 13:55:28 +0300

    Major changes cba to comment

--- link.py
+++ link.py
@@ -1,5 +1,7 @@
 from crypt import methods
-from flask import Flask, render_template, request
+from distutils.dep_util import newer
+from telnetlib import DO
+from flask import Flask, render_template, request, send_from_directory
 import os
 from flask_bootstrap import Bootstrap
 from main import traverse
@@ -22,21 +24,35 @@ BASIC_PATH = os.getcwd()
 
 UPLOAD_PATH = os.path.join(BASIC_PATH, 'upload/')
 
+DOWNLOAD_PATH = os.path.join(BASIC_PATH, 'modified_files/')
 
 
 @app.route('/', methods=['POST', 'GET'])
 def network():
     if request.method == 'POST':
+        print("LINK CHECK")
         uploaded_xlsx = request.files['file']
         file_name = uploaded_xlsx.filename
         save_path = os.path.join(UPLOAD_PATH, file_name)
         uploaded_xlsx.save(save_path)
-        print(file_name, "Uploaded Successfully")
-        traverse(file_name)
+        new = file_name.split('.')
+        del new[-1]
+        new = ''.join(new)
+        traverse(new)
+        os.remove(UPLOAD_PATH + new + '.xlsx')
+        return render_template('bounce.html',new = new)
+        
     else:
         return render_template('template1.html')
 
-        
+
+
+@app.route('/download/<variable>/', methods=['POST', 'GET'])
+def downloading(variable):
+    return send_from_directory(DOWNLOAD_PATH, 'improved_' + variable + '.csv'), os.remove(DOWNLOAD_PATH + 'improved_' + variable + '.csv')
+    # os.remove(DOWNLOAD_PATH + 'improved_' + variable + '.csv')
+    # return render_template('template1.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import pandas as pd
 import phonenumbers
 from phonenumbers.carrier import name_for_number
+import os
 
 
 def traverse(filename):
-    df = pd.read_excel(filename)
+    df = pd.read_excel('upload/' + filename + '.xlsx')
     temp_list = []
+    print('MAIN CHECK')
     for index, row in df.iterrows():
         if len(str(row['Number'])) < 11:
             v = df.iloc[index]
@@ -23,4 +25,6 @@ def traverse(filename):
         temp_list.append(new_carrier)
 
     df['New Carrier'] = temp_list
-    df.to_csv('modified.csv', index = False)
+    # # df.reset_index(drop =True, inplace=True)
+    # # df.to_csv(r'BASIC_PATH + /modified_files\modified.csv'  ,index = False)
+    df.to_csv(os.path.join('modified_files','improved_' + filename + '.csv'))
